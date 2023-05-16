866cbce8843b552389123f0219487a39121228df
maneeshaeduvaka <107666668+maneeshaeduvaka@users.noreply.github.com>
2023-05-16 14:39:38 +0530

    Add files via upload
     Create a function, pass two parameters in it and print product of the two numbers.

---
+++ Day2/productofarguments.js
@@ -0,0 +1,8 @@
+//Create a function, pass two parameters in it and print product of the two numbers.
+function product(a,b){
+    return a*b;
+}
+console.log(product(2,5));
+//arrow function
+let pro = (x,y) => x*y;
+console.log(pro(3,9));
